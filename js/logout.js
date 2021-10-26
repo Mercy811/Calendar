44cf637fa24225dbd261a8c97dabc4e9b41fb121
Mercy811 <xinyiye811@gmail.com>
2021-10-26 18:32:00 -0500

    [fix] logout clean events

--- js/logout.js
+++ js/logout.js
@@ -1,5 +1,10 @@
 document.getElementById("logout-btn").addEventListener("click", function (event) {
 
+    let events = document.getElementsByClassName("event-btn");
+    while(events.length>0){
+        events[0].parentNode.removeChild(events[0]);
+    }
+
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.getElementById("login-container").style.display = "block";
     document.getElementById("calendar-container").style.display = "none";
